44e1c0badcdda35d846005dcafd111d296bc028f
MaribelMOA <maribel.mendez@uabc.edu.mx>
2022-09-01 18:35:59 -0700

    Agregue la clase Task.

---
+++ src/Pack/Task.java
@@ -0,0 +1,44 @@
+public class Task {
+
+	private String name;
+	private int theEmployees;
+	private int theResources;
+
+
+	public void getName(String name) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(String x) {
+		this.name = x;
+	}
+
+	public void getTheEmployees() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param theEMployees
+	 */
+	public void setTheEmployees(int y) {
+		this.theEmployees = y;
+	}
+
+	public void getTheResources() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param theResources
+	 */
+	public void setTheResources(int z) {
+		this.theResources = z;
+	}
+
+}
