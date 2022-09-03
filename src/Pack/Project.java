b762409b628ccaa66ccbc9d7e9bea03c27bc8dbc
marco <marco.leon11@uabc.du.mx>
2022-09-03 11:24:26 -0700

    Agregué Project

---
+++ src/Pack/Project.java
@@ -0,0 +1,61 @@
+package Diagrama2.ProjectManagment;
+
+public class Project {
+
+	private String name;
+	private int theResources;
+	private int theEmployees;
+	private int theWorkBreakdownStructure;
+
+	public void getName() {
+		return this.name;
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void getTheResources() {
+		// TODO - implement Project.getTheResources
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param theResources
+	 */
+	public void setTheResources(int theResources) {
+		this.theResources = theResources;
+	}
+
+	public void getTheEmployees() {
+		// TODO - implement Project.getTheEmployees
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param theEmployees
+	 */
+	public void setTheEmployees(int theEmployees) {
+		this.theEmployees = theEmployees;
+	}
+
+	public void getTheWorkBreakdownStructure() {
+		// TODO - implement Project.getTheWorkBreakdownStructure
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param theWordBreakdownStructure
+	 */
+	public void setTheWorkBreakdownStructure(int theWordBreakdownStructure) {
+		this.theWorkBreakdownStructure = theWordBreakdownStructure;
+	}
+
+}
